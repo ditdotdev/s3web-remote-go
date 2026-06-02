96a92580ba94d747daeb1aee81cc2c450a454d94
Robert Reeves <robertericreeves@gmail.com>
2026-06-02 12:12:08 -0500

    Rename datadatdat to dit: module github.com/ditdotdev/s3web-remote-go (#49)
    Module path + imports + org refs -> ditdotdev; depend on released
    github.com/ditdotdev/remote-sdk-go v1.9.3. Workflows, CODEOWNERS, docs branding.

--- cmd/s3web/main.go
+++ cmd/s3web/main.go
@@ -1,7 +1,7 @@
 // Package main provides the s3web remote server executable.
 package main
 
-import "github.com/datadatdat/remote-sdk-go/remote"
+import "github.com/ditdotdev/remote-sdk-go/remote"
 
 func main() {
 	remote.Serve("s3web")
